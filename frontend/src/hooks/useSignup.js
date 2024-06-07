0bfbb21e8e9d14ea0dba055aec7d04ff3a6889a1
Solomon Ebata <solomone724@gmail.com>
2024-06-07 17:57:23 -0400

    fixed signup bug

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
+import { useNavigate } from "react-router-dom";
 
 export const useSignup = () => {
   // States
   const [error, setError] = useState(null)
   const [isLoading, setisLoading] = useState(null)
   const { dispatch } = useAuthContext()
+  const navigate = useNavigate();
 
   const signup = async (name, email, password) => {
     setisLoading(true)
@@ -38,7 +40,8 @@ export const useSignup = () => {
       //dispatch({ type: 'LOGIN', payload: json })
 
       // if response is ok, move to login page
-      window.location.href = '/login'
+      //window.location.href = '/login' // this only works in development
+      navigate('/login')
 
       setisLoading(false)
     }
